cd22292b71c8d37c019f4174c44741d8705d48ac
Loren <lorenzo.perez@ing.unlp.edu.ar>
2021-02-26 09:29:24 -0300

    chore: improved extraction regex, added 320 missing entries to catalog (Total 1476)

--- scripts/extract_catalog.mjs
+++ scripts/extract_catalog.mjs
@@ -6,38 +6,45 @@ const lines = text.split('\n');
 const catalog = [];
 let currentEntry = null;
 
-// The text starts the main numbered entries around line 1000.
-// We look for lines matching: ^\s*(\d+)\.\s+([A-Z\s]+)(?:.-|.)
-const entryRegex = /^\s*(\d+)\.\s+([A-Z][A-Z\s,]+(?:.-|.)?)(.*)/;
+// More permissive regex
+const entryRegex = /^\s*(\d+[a-zA-Z\s]*)\.\s+(.*)/;
 
 for (let i = 0; i < lines.length; i++) {
   const line = lines[i];
   const match = line.match(entryRegex);
   
   if (match) {
-    // If we have an existing entry, save it
     if (currentEntry) {
       catalog.push(currentEntry);
     }
     
-    const num = match[1];
-    let title = match[2].trim();
-    // Clean up trailing dashes or dots
-    title = title.replace(/[-.\s]+$/, '');
+    // Clean up number
+    let numStr = match[1].replace(/\s+/g, '').toLowerCase();
     
-    let description = match[3] ? match[3].trim() : '';
+    // The rest of the line is title and potentially description
+    let rest = match[2].trim();
+    let title = rest;
+    let description = '';
+    
+    // Attempt to split title and description
+    // Titles are usually uppercase and end with a dash or dot.
+    const titleMatch = rest.match(/^([A-Z0-9\s,\-&'"]+)(?:.-|\.|-)(.*)/);
+    if (titleMatch) {
+      title = titleMatch[1].trim();
+      description = titleMatch[2].trim();
+    }
     
     currentEntry = {
-      id: `HISCOX-${num.padStart(4, '0')}`,
+      id: `HISCOX-${numStr.padStart(4, '0')}`,
       title: title,
       families: [],
       degreesOfFreedom: 0,
-      fidelity: "STATIC", // default for extraction
+      fidelity: "STATIC", 
       source: {
         edition: "1899",
         year: 1899,
-        page: 0, // Need manual or PDF correlation
-        figureNumber: num,
+        page: 0,
+        figureNumber: numStr,
         originalTitle: title,
         originalDescription: description,
         url: "https://archive.org/details/1800-mechanical-movements-devices-and-appliances_202005"
@@ -52,35 +59,34 @@ for (let i = 0; i < lines.length; i++) {
       solver: { type: "DIRECT" }
     };
   } else if (currentEntry && line.trim() !== '') {
-    // Append to description if it's the following lines, until we hit a blank line or a new entry
-    // We only want a short snippet, maybe max 300 chars
-    if (currentEntry.source.originalDescription.length < 300 && !line.match(/^[A-Z\s]{10,}$/)) {
+    // Append to description if it's not a capitalized heading or index
+    if (currentEntry.source.originalDescription.length < 500 && !line.match(/^[A-Z\s]{10,}$/)) {
         currentEntry.source.originalDescription += " " + line.trim();
     }
   }
 }
 
-// Push last entry
 if (currentEntry) {
   catalog.push(currentEntry);
 }
 
-// We only want 1 to 1800. Let's filter out false positives (numbers > 2000 or duplicate numbers).
 const filtered = [];
 const seen = new Set();
 for (const entry of catalog) {
-  const num = parseInt(entry.id.split('-')[1], 10);
-  if (num > 0 && num <= 1800 && !seen.has(num)) {
-    // Further cleanup of description formatting
-    entry.source.originalDescription = entry.source.originalDescription.replace(/\s+/g, ' ').trim();
-    filtered.push(entry);
-    seen.add(num);
+  // Only keep things that look like numbers 1-1800
+  const match = entry.id.match(/HISCOX-0*(\d+)/);
+  if (match) {
+    const num = parseInt(match[1], 10);
+    if (num > 0 && num <= 1800 && !seen.has(entry.id)) {
+      entry.source.originalDescription = entry.source.originalDescription.replace(/\s+/g, ' ').trim();
+      filtered.push(entry);
+      seen.add(entry.id);
+    }
   }
 }
 
 console.log(`Extracted ${filtered.length} entries.`);
 
-// Merge with existing catalog.json to not overwrite my hand-crafted entries
 const existingPath = 'src/data/catalog.json';
 let existing = [];
 if (fs.existsSync(existingPath)) {
@@ -88,15 +94,16 @@ if (fs.existsSync(existingPath)) {
 }
 
 const existingMap = new Map(existing.map(e => [e.id, e]));
+let added = 0;
 
 for (const entry of filtered) {
   if (!existingMap.has(entry.id)) {
     existing.push(entry);
+    added++;
   }
 }
 
-// Sort by ID
 existing.sort((a, b) => a.id.localeCompare(b.id));
 
 fs.writeFileSync(existingPath, JSON.stringify(existing, null, 2));
-console.log('Updated src/data/catalog.json');
+console.log(`Added ${added} new entries to catalog.json. Total is now ${existing.length}.`);
